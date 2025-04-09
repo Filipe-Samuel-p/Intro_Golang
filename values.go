023ef88805e38c15faaacfcbbff01c95cd58a4cd
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-04-09 17:17:10 -0300

    sintaxe

---
+++ values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("Go" + "land") // contatenação
+	fmt.Println(true && false)
+	fmt.Println(false || true)
+	fmt.Println(!true)
+
+}
