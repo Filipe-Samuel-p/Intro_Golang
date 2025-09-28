a68bdf642e1ac457769a6ed902cebdb42f8f3637
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-09-27 22:23:19 -0300

    basic structures: Repetition

--- Basic structures/Repetition Structures/ex1/ex1.go
+++ Basic structures/Repetition Structures/ex1/ex1.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	var total int
+
+	fmt.Print("Quantos múltiplos de 3 quer ver: ")
+	fmt.Scan(&total)
+
+	for i := 1; i <= total; i++ {
+		fmt.Println(i * 3)
+	}
+}
---
+++ Basic structures/Repetition Structures/ex2/ex2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var count int = 10
+
+	for count >= 0 { // Simulação de um While
+		fmt.Println(count)
+		count--
+	}
+	fmt.Print("FIM!!!")
+
+}
